2972bd567eaad7614027c3557dad36ed5955cab0
RainNy99 <RainNy99@gmail.com>
2021-06-13 03:39:47 +0700

    send Info

---
+++ SpringBook_BookWeb/springboot/src/main/java/com/ht/controller/SendMailController.java
@@ -0,0 +1,63 @@
+package com.ht.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SendMailController {
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@PostMapping("/checkout")
+	public String sendTotal(HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
+		String name = request.getParameter("receiptName");
+		String mail = request.getParameter("receiptMail");
+		String phone = request.getParameter("receiptPhone");
+		String address = request.getParameter("receiptAddress");
+		String note = request.getParameter("note");
+		String total = request.getParameter("total");
+
+//		SimpleMailMessage message = new SimpleMailMessage();
+
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+		String mailSubject = "Thông tin đơn hàng";
+		String mailContent = "<p><b>Người nhận: </b>" + name + "</p>";
+		mailContent += "<p><b>Email: </b>" + mail + "</p>";
+		mailContent += "<p><b>Số điện thoại: </b>" + phone + "</p>";
+		mailContent += "<p><b>Địa chỉ: </b>" + address + "</p>";
+		mailContent += "<p><b>Ghi chú: </b>" + note + "</p>";
+//		mailContent += "<p><b>Thanh toán: </b>" + total+ "</p>"
+		mailContent += "<p>Đơn hàng đang tiến hành đóng gói để giao đến bạn.<p>"
+				+ "Cảm ơn bạn đã tin tưởng và ủng hộ Writer!</p>";
+		mailContent += "<br><h3>Bạn có thể liên hệ với WRITER về đơn hàng bằng các cách sau:</h3>"
+				+ "<p>	- Gọi đến Hotline (+800) 123 4567 890</p>"
+				+ "<p>	- Gửi mail về địa chỉ: info@bookstore.com</p>"
+				+ "<p>Chúc bạn có một ngày mới vui vẻ!</p>";
+		
+		mailContent += "<img src='cid:logoImage'/>";
+
+		helper.setFrom("hoaitugl@gmail.com", "WRITER Order");
+		helper.setTo(mail);
+		helper.setSubject(mailSubject);
+		helper.setText(mailContent, true);
+		mailSender.send(message);
+		
+		ClassPathResource resource = new ClassPathResource("/static/resource/user/img/logo.png");
+		helper.addInline("logoImage", resource);
+		
+		return "success";
+	}
+}
